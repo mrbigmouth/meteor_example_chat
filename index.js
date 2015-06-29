9a7e125e57cfd3bcc859a2dfaa071d541b9162a6
Yong Qian, Lee <b92101007@gmail.com>
2015-06-29 21:29:26 +0800

    加入參數的驗證機制與Meteor.call時錯誤的細節顯示機制

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ messageList = new Meteor.Collection('messageList');
 Meteor.methods({
   addMessage: function(speaker, messageText) {
       var message = {};
+      //防止惡意操作的型別檢查
+      if (Meteor.isServer) {
+        check(speaker, String);
+        check(messageText, String);
+      }
+      if (speaker.length < 1 || speaker.length > 20) {
+        throw new Meteor.Error(403, '使用者的名稱不合法', '使用者名稱應至少一個字元，至多二十個字元！');
+      }
+      if (messageText.length < 1 || messageText.length > 255) {
+        throw new Meteor.Error(403, '發言內容不合法', '發言內容應至少一個字元，至多兩百五十五個字元！');
+      }
       message.speaker = speaker;
       message.message = messageText;
       //在使用者端執行的時候，時間自動視為最新message的時間+1秒
@@ -40,8 +51,12 @@ if (Meteor.isClient) {
   Template.chat_input.events({
     'submit': function(e, ins) {
       var form = ins.firstNode;
-      //呼叫addMessage方法
-      Meteor.call('addMessage', userName.get(), form.message.value);
+      //呼叫addMessage方法，並產生錯誤時以alert顯示錯誤細節
+      Meteor.call('addMessage', userName.get(), form.message.value, function(error) {
+        if (error) {
+          alert(error.reason + '\n' + error.details);
+        }
+      });
       //發完言後把輸入框裡的值清空
       form.message.value = '';
       //防止表單的預設送出動作
