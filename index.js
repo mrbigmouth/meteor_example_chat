2630a94aa44aaf72bdf3b0a21893f8fda3db96a4
Yong Qian, Lee <b92101007@gmail.com>
2015-07-01 07:21:12 +0800

    建立帳號系統

--- index.js
+++ index.js
@@ -2,20 +2,17 @@
 messageList = new Meteor.Collection('messageList');
 //建立addMessage這個伺服器端與使用者端通用的方法
 Meteor.methods({
-  addMessage: function(speaker, messageText) {
+  addMessage: function(messageText) {
       var message = {};
       //防止惡意操作的型別檢查
       if (Meteor.isServer) {
-        check(speaker, String);
         check(messageText, String);
-      }
-      if (speaker.length < 1 || speaker.length > 20) {
-        throw new Meteor.Error(403, '使用者的名稱不合法', '使用者名稱應至少一個字元，至多二十個字元！');
       }
       if (messageText.length < 1 || messageText.length > 255) {
         throw new Meteor.Error(403, '發言內容不合法', '發言內容應至少一個字元，至多兩百五十五個字元！');
       }
-      message.speaker = speaker;
+      //設定發言者為當前登入的使用者id
+      message.user = this.userId;
       message.message = messageText;
       //在使用者端執行的時候，時間自動視為最新message的時間+1秒
       if (Meteor.isClient) {
@@ -32,27 +29,40 @@ Meteor.methods({
 });
 
 if (Meteor.isClient) {
-  //使用者暱稱預設為無名氏
-  var userName = new ReactiveVar('無名氏');
-  //當Meteor啟動時，詢問使用者暱稱
-  Meteor.startup(function() {
-    var inputUserName = window.prompt('請輸入您的暱稱');
-    if (inputUserName) {
-      userName.set( inputUserName );
+  //註冊廣域helper -- 根據使用者id取得使用者暱稱
+  Template.registerHelper('getUserNickName', function(userId) {
+    var user = Meteor.users.findOne(userId);
+    //預設使用者暱稱為無名氏
+    var userNickName = '無名氏';
+    //若存在使用者且有設定使用者暱稱，則改為使用使用者設定的暱稱
+    if (user && user.profile && user.profile.nickName) {
+      userNickName = user.profile.nickName;
     }
+    return userNickName;
   });
-  //設定chat_input的helper
-  Template.chat_input.helpers({
-    userName: function() {
-      return userName.get();
-    }
-  });
-  //設定chat_input的發言事件
+
   Template.chat_input.events({
+    //設定使用者暱稱
+    'click button': function(e, ins) {
+      var userNickName = prompt('請輸入新的暱稱:');
+      if (userNickName.length > 20 || userNickName < 1) {
+        alert('使用者暱稱不可為空白、長度不可超過二十個字元！')
+        return false;
+      }
+      //直接修改使用者暱稱
+      Meteor.users.update(Meteor.userId(), {
+        $set: {
+          profile: {
+            nickName: userNickName
+          }
+        }
+      });
+    },
+    //處理註冊使用者發言
     'submit': function(e, ins) {
       var form = ins.firstNode;
       //呼叫addMessage方法，並產生錯誤時以alert顯示錯誤細節
-      Meteor.call('addMessage', userName.get(), form.message.value, function(error) {
+      Meteor.call('addMessage', form.message.value, function(error) {
         if (error) {
           alert(error.reason + '\n' + error.details);
         }
